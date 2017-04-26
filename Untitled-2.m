bd3317f507c714e55d03d6c8387404c06400a097
arpanrau <arpan.rau@gmail.com>
2017-04-26 10:26:54 -0400

    Module does not work :_(

---
+++ Untitled-2.m
@@ -0,0 +1,25 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*answergenerate[x0_]:=*)
+(*Module[{Variablesleft,Lessthanfifty,nonzero,nonzero2},*)
+(*Answer = Solve[x0 ,x];*)
+(*Variablesleft = Variables[First[Answer]];*)
+(*Lessthanfifty = Reduce[Abs[Variablesleft] < 50 ];*)
+(*nonzero = Reduce[Variablesleft < 0];*)
+(*  nonzero2 = Reduce[Variablesleft > 0];*)
+(*     Return[FindInstance[answer && Lessthanfifty &&  nonzero2,Variablesleft,Integers]]*)
+(*]*)
+
+
+(* ::Input:: *)
+(*answergenerate[a x^2 + b x + c ==0]*)
+(**)
+
+
+(* ::InheritFromParent:: *)
+(**)
+
+
+(* ::InheritFromParent:: *)
+(**)
---
+++ why.m
@@ -0,0 +1,19 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*answergenerate[x0_]:=*)
+(*Module[{Variablesleft,Lessthanfifty,nonzero,nonzero2},*)
+(*Answer = Solve[x0 ,x];*)
+(*Variablesleft = Variables[First[Answer]];*)
+(*Lessthanfifty = Reduce[Abs[Variablesleft] < 50 ];*)
+(*nonzero = Reduce[Variablesleft < 0];*)
+(*  nonzero2 = Reduce[Variablesleft > 0];*)
+(*     Return[FindInstance[Answer && Lessthanfifty && (nonzero ||  nonzero2),Variablesleft,Integers]]*)
+(*]*)
+
+
+(* ::Input:: *)
+(*answergenerate[a x^2 + b x + c ==0]*)
+
+
+
